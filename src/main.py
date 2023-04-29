3722e9d13da46663192288f77f5d98d3bf3d9ed4
saktush <sergey.aktush@gmail.com>
2023-04-29 18:08:58 +0300

    improvved input, full functionality

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import streamlit as st
-from decouple import config
 import openai
+from streamlit.report_thread import get_report_ctx
+from streamlit.server.server import Server
 
 response = False
 prompt_tokens = 0
 completion_tokes = 0
 total_tokens_used = 0
 cost_of_response = 0
+st.session_state.triggered_enter = False
 
-API_KEY = config('OPENAI_API_KEY')
+API_KEY = ''
 openai.api_key = API_KEY
 
 
@@ -16,30 +18,25 @@ def make_request(question_input: str):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"{question_input}"},
+            {"role": "user", "content": f"{question_input}"},
         ]
     )
     return response
 
 
 st.header("Streamlit + OpenAI ChatGPT API")
-
 st.markdown("""---""")
 
-question_input = st.text_input("Enter question")
-rerun_button = st.button("Rerun")
+with st.form(key='my_form'):
+    question_input = st.text_area("Enter question and push Enter to send", height=200)
+    submit_button = st.form_submit_button(label='Submit',
+                                          help="Press CMD/Ctrl + Enter to send the message")
+    if submit_button or st.session_state.triggered_enter:
+        response = make_request(question_input)
+    else:
+        pass
 
 st.markdown("""---""")
-
-if question_input:
-    response = make_request(question_input)
-else:
-    pass
-
-if rerun_button:
-    response = make_request(question_input)
-else:
-    pass
 
 if response:
     st.write("Response:")
@@ -61,3 +58,10 @@ with st.sidebar:
     st.write("Completion tokens used :", completion_tokes)
     st.write("Total tokens used :", total_tokens_used)
     st.write("Total cost of request: ${:.8f}".format(cost_of_response))
+
+if st.session_state.triggered_enter:
+    st.info("Hit Enter to send the message")
+
+if __name__ == '__main__':
+    print("To run this script type:\nstreamlit run src/main.py")
+    pass
