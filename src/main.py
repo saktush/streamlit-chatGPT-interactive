2fdb105e301cd12f1f55e3d8439d538177fe3c0f
saktush <sergey.aktush@gmail.com>
2023-04-29 22:22:14 +0300

    setting up APPI key storage

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 import streamlit as st
 import openai
-from streamlit.report_thread import get_report_ctx
-from streamlit.server.server import Server
 
-response = False
-prompt_tokens = 0
-completion_tokes = 0
-total_tokens_used = 0
-cost_of_response = 0
-st.session_state.triggered_enter = False
+API_KEY = "{text=''}"
 
-API_KEY = ''
-openai.api_key = API_KEY
+with open("API_KEY.txt", "r") as key:
+    _key = key.read()[0]
+    API_KEY.format(_key)
 
 
 def make_request(question_input: str):
@@ -24,44 +18,52 @@ def make_request(question_input: str):
     return response
 
 
-st.header("Streamlit + OpenAI ChatGPT API")
-st.markdown("""---""")
+def main():
+    # set basics
+    response = False
+    prompt_tokens = 0
+    completion_tokes = 0
+    total_tokens_used = 0
+    cost_of_response = 0
+    st.session_state.triggered_enter = False
 
-with st.form(key='my_form'):
-    question_input = st.text_area("Enter question and push Enter to send", height=200)
-    submit_button = st.form_submit_button(label='Submit',
-                                          help="Press CMD/Ctrl + Enter to send the message")
-    if submit_button or st.session_state.triggered_enter:
-        response = make_request(question_input)
+    # set header
+    st.header("Streamlit + OpenAI ChatGPT API")
+    st.markdown("""---""")
+
+    with st.form(key='my_form'):
+        question_input = st.text_area("Enter question and push Enter to send", height=200)
+        submit_button = st.form_submit_button(label='Submit',
+                                              help="Press CMD/Ctrl + Enter to send the message")
+        if submit_button or st.session_state.triggered_enter:
+            response = make_request(question_input)
+        else:
+            pass
+
+    st.markdown("""---""")
+
+    if response:
+        st.write("Response:")
+        st.write(response["choices"][0]["message"]["content"])
+
+        prompt_tokens = response["usage"]["prompt_tokens"]
+        completion_tokes = response["usage"]["completion_tokens"]
+        total_tokens_used = response["usage"]["total_tokens"]
+
+        cost_of_response = total_tokens_used * 0.000002
     else:
         pass
 
-st.markdown("""---""")
+    with st.sidebar:
+        st.title("Usage Stats:")
+        st.markdown("""---""")
+        st.write("Promt tokens used :", prompt_tokens)
+        st.write("Completion tokens used :", completion_tokes)
+        st.write("Total tokens used :", total_tokens_used)
+        st.write("Total cost of request: ${:.8f}".format(cost_of_response))
 
-if response:
-    st.write("Response:")
-    st.write(response["choices"][0]["message"]["content"])
-
-    prompt_tokens = response["usage"]["prompt_tokens"]
-    completion_tokes = response["usage"]["completion_tokens"]
-    total_tokens_used = response["usage"]["total_tokens"]
-
-    cost_of_response = total_tokens_used * 0.000002
-else:
-    pass
-
-
-with st.sidebar:
-    st.title("Usage Stats:")
-    st.markdown("""---""")
-    st.write("Promt tokens used :", prompt_tokens)
-    st.write("Completion tokens used :", completion_tokes)
-    st.write("Total tokens used :", total_tokens_used)
-    st.write("Total cost of request: ${:.8f}".format(cost_of_response))
-
-if st.session_state.triggered_enter:
-    st.info("Hit Enter to send the message")
 
 if __name__ == '__main__':
-    print("To run this script type:\nstreamlit run src/main.py")
+    main()
+    # print("To run this script type:\nstreamlit run src/main.py")
     pass
