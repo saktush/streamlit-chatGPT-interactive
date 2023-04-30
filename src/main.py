7aa8dfeaee83cced2c152de12dbb50b50587435a
saktush <sergey.aktush@gmail.com>
2023-04-30 18:07:06 +0300

    form optimization

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import openai
-import os
-import sys
-from typing import Dict, List
 
 
 def make_request(question_input: str):
@@ -22,17 +19,28 @@ def main():
     completion_tokes = 0
     total_tokens_used = 0
     cost_of_response = 0
-    # st.session_state.triggered_enter = False
+    st.session_state.triggered_enter = False
+    st.set_page_config(
+        page_title="Ex-stream-ly Cool App",
+        page_icon="🧊",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug",
+            'About': "# This is a header. This is an *extremely* cool app!"
+        }
+    )
 
     # set header
     st.header("OpenAI ChatGPT API interface")
 
     # Display response in scrollable area
-    response_area = st.empty()
 
     # st.markdown("""---""")
     with st.form(key='request_form'):
-        question_input = st.text_area("Enter question and push Enter to send", height=150)
+        response_area = st.empty()
+        question_input = st.text_area("Enter question and push Submit", height=150)
         submit_button = st.form_submit_button(label='Submit',
                                               help="Press CMD/Ctrl + Enter to send the message",
                                               )
@@ -41,16 +49,17 @@ def main():
         else:
             pass
 
-    if response:
-        response_text = response["choices"][0]["message"]["content"]
-        response_area.text_area(label="Response", value=response_text, height=150)
-        st.markdown("""---""")
+        # show response
+        if response:
+            response_text = response["choices"][0]["message"]["content"]
+            response_area.text_area(label="Response", value=response_text, height=150)
+            st.markdown("""---""")
 
-        prompt_tokens = response["usage"]["prompt_tokens"]
-        completion_tokes = response["usage"]["completion_tokens"]
-        total_tokens_used = response["usage"]["total_tokens"]
+            prompt_tokens = response["usage"]["prompt_tokens"]
+            completion_tokes = response["usage"]["completion_tokens"]
+            total_tokens_used = response["usage"]["total_tokens"]
 
-        cost_of_response = total_tokens_used * 0.000002
+            cost_of_response = total_tokens_used * 0.000002
 
     with st.sidebar:
         st.title("Usage Stats:")
@@ -62,8 +71,9 @@ def main():
 
 
 if __name__ == '__main__':
+    # TODO: API KEY IMPORT - it will be grate to import api key from some app
     API_KEY = ""
     openai.api_key = API_KEY
-
+    # TODO: think about running code with shell file
     main()
     pass
