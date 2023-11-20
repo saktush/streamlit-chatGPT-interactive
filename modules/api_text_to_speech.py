c816c1568cdef6ecb792680b1e3fdb569c266833
aktush <sergey.aktush@ya.ru>
2023-11-21 01:13:01 +0300

    major update

--- modules/api_text_to_speech.py
+++ modules/api_text_to_speech.py
@@ -1,30 +1,46 @@
+import threading
 from pathlib import Path
 from openai import OpenAI
 from decouple import config
 
-text = """
-Чтобы нам не ошибаться,
-Надо правильно прочесть:
-Три, четырнадцать, пятнадцать,
-Девяносто два и шесть.
-Ну и дальше надо знать,
-Если мы вас спросим —
-Это будет пять, три, пять,
-Восемь, девять, восемь.
+# Test text variable
+test_text = """
+Hello, darkness, my old friend,
+I've come to talk with you again
+Because a vision softly creeping,
+Left its seeds while I was sleeping,
+And the vision that was planted in my brain
+Still remains within the sound of silence.
 """
-
-client = OpenAI(
-    api_key=config("OPENAI_API_KEY")
-)
-
 Voice_list = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
 
-for v in Voice_list[-2:]:
-    speech_file_path = Path(__file__).parent / "result" / f"P_audio_{v}.mp3"
-    response = client.audio.speech.create(
-      model="tts-1",
-      voice=v,
-      input=text
-    )
+# initialize openai client
+client = OpenAI(api_key=config("OPENAI_API_KEY"))
 
+
+def generate_speech(voice, text, name_prefix="text") -> None:
+    speech_file_path = (
+        Path(__file__).parent / "result" / f"{name_prefix}_audio_{voice}.mp3"
+    )
+    response = client.audio.speech.create(
+        model="tts-1-hd", voice=voice, input=text, speed=1
+    )
     response.stream_to_file(speech_file_path)
+
+
+# Create a lock to ensure only one thread is active at a time
+thread_lock = threading.Lock()
+
+
+for v in Voice_list:
+    # Acquire the lock before starting the thread
+    with thread_lock:
+        # Start a thread for each voice
+        thread = threading.Thread(
+            target=generate_speech,
+            args=(v, test_text, "EN"),
+        )
+        thread.start()
+
+    # Wait for the thread to finish before moving to the next iteration
+    thread.join()
